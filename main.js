395ab251f0fb5ff91918d23cff3cc0bc4a3a29a6
Harpreet Singh <designsbyharp@gmail.com>
2019-12-15 14:49:11 -0500

    Completed

---
+++ main.js
@@ -0,0 +1,49 @@
+let dateLastYear = (function(){
+
+    function getTodaysDate(){
+
+        let todaysDate, day, days;
+
+        todaysDate = new Date();
+        todaysDate.setFullYear(todaysDate.getFullYear() -1);
+        day = todaysDate.getDay();
+        days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+        lastYearsDay = days[day - 1];
+
+        return lastYearsDay;
+
+    }
+
+    function popUpDomEl(){
+        let jsPopUpDomEl = document.querySelector("#time-popup__header"),
+          jsTimePopup = document.querySelector("#time-popup");
+        
+        return {
+            h1Popup: jsPopUpDomEl,
+            divTimePopup: jsTimePopup
+        };
+    }
+
+    function dayMessage(){
+        let day = getTodaysDate(),
+            jsPopUpDomEl = popUpDomEl().h1Popup,
+            jsTimePopUpDomEl = popUpDomEl().divTimePopup;
+
+        jsPopUpDomEl.textContent = day; 
+        jsTimePopUpDomEl.classList.remove("is-hidden");
+    }
+
+    // function displayBlock(){
+    //     let popUpDomEl = popUpDomEl();
+    //     popUpDomEl.classList.add(".u-display-block");
+    // }
+
+    return{
+        getDay: dayMessage
+    }
+
+})();
+
+document.querySelector("#cta-time").addEventListener("click", function(){
+    dateLastYear.getDay();
+});
